5c8058ad5e7668e3349703cdd68501866ac864f5
Zando <123271994+Zando411@users.noreply.github.com>
2024-05-31 23:43:24 -0700

    add beginning text and pregame text menu

--- diceSlayer.ino
+++ diceSlayer.ino
@@ -5,18 +5,46 @@
 
 AsyncDelay animationTimer;
 
+
+// player variables/upgrades (inital values are defaults)
+int playerMaxHP = 3;
+int playerCurrentHP = playerMaxHP;
+int playerMaxRoll = 6;
+int playerRollPlus = 0;
+bool rerollPerk = false;
+bool remainingReroll = false;
+int playerRoll;
+
+// current monster variables
+int monsterHP = 3;
+int monsterMaxRoll = 6;
+int monsterRollPlus = 0;
+int monsterRoll;
+
+// function variables
 #define DICE_COLOR          0xEA6292    // Dice digits color
-
-unsigned long rollStartTime;
-bool rightButton;
-uint8_t rollNumber;
-
-
-void buttonCallback() {
-  rightButton = true;
+bool rightButtonPressed;
+bool leftButtonPressed;
+bool switchChange;
+int rollNumber;
+int rightButtonPin = 5;
+int leftButtonPin = 4;
+int switchPin = 7;
+bool endlessMode = false;
+bool gameStarted = false;
+bool combatOver = false;
+// ISR
+void rightISR() {
+  rightButtonPressed = true;
 }
-
-uint8_t dicePixels[6][6] = {  // Pixel pattern for dice roll
+void leftISR() {
+  leftButtonPressed = true;
+}
+void switchISR() {
+  switchChange = true;
+}
+// Dice array
+int dicePixels[6][6] = {  // Pixel pattern for dice roll
   { 2, 0, 0, 0, 0, 0 } ,      // Roll = 1
   { 4, 9, 0, 0, 0, 0 } ,      //        2
   { 0, 4, 7, 0, 0, 0 } ,      //        3
@@ -24,23 +52,52 @@ uint8_t dicePixels[6][6] = {  // Pixel pattern for dice roll
   { 0, 2, 4, 5, 9, 0 } ,      //        5
   { 0, 2, 4, 5, 7, 9 } ,      //        6
 };
+// Monster arrays
 
 void setup() {
   Serial.begin(9600);
-  
   CircuitPlayground.begin();
-  CircuitPlayground.setBrightness(100);
+  CircuitPlayground.setBrightness(50);
   // Seed the random function with light sensor value
-  attachInterrupt(digitalPinToInterrupt(5), buttonCallback, RISING);
-  // Initialize the global states
+  attachInterrupt(digitalPinToInterrupt(rightButtonPin), rightISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(leftButtonPin), leftISR, RISING);
+  attachInterrupt(digitalPinToInterrupt(switchPin), switchISR, CHANGE);
+  
+  while(!Serial); 
+  delay(500);
+  Serial.println("Welcome to Dice Slayer!");
+  delay(1000);
+  Serial.println("Please select a mode by changing the switch.");
+  delay(1000);
+  Serial.println("Press the right button to begin!");
 }
 
 void loop() {
-  if (rightButton) {
+  if (!gameStarted) { // check if game has not started
+    if (switchChange) {
     delay(5);
-    rollDice();
+    endlessMode = digitalRead(switchPin); // set game mode based on switch
+    
+    Serial.print("You have selected: ");
+    if (endlessMode) Serial.println("Endless Mode");
+    if (!endlessMode) Serial.println("Story Mode");
+
+    switchChange = false;
   }
-  Serial.println(rollNumber);
+  if (rightButtonPressed) {
+    delay(300);
+    Serial.println("Let the game begin!");
+    gameStarted = true;
+    rightButtonPressed = false;
+  }
+  }
+  if (gameStarted) {
+    if (!endlessMode) { // Story mode
+    
+  }
+  }
+  
+  
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -55,18 +112,17 @@ int rollDice() {
   randomSeed(CircuitPlayground.lightSensor());
    bool loop = false;
 
-  if (rightButton) {
-    rollStartTime = millis();
-    rightButton = false;
+  if (rightButtonPressed) {
+    rightButtonPressed = false;
     animationTimer.start(1000, AsyncDelay::MILLIS);
   }
   
   // Compute a random number from 1 to 6
   rollNumber = random(1,7);
 
-if (!(animationTimer.isExpired())) {
-  loop = true;
-}
+  if (!(animationTimer.isExpired())) {
+    loop = true;
+  }
 
   // Display status on NeoPixels
   while (loop) {
